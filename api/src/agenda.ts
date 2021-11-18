4fdeb073a1649e29956d1b2536f014b7f00e9991
Stefan <stefanmitov8@gmail.com>
2021-11-18 19:15:17 +0200

    ensure agenda is running before app start

---
+++ api/src/agenda.ts
@@ -0,0 +1,13 @@
+import Agenda from 'agenda';
+
+if (!process.env.CONNECTION_STRING) {
+  throw new Error('No mongoDB connection string provided!');
+}
+
+const agenda = new Agenda({
+  db: {
+    address: process.env.CONNECTION_STRING,
+  },
+});
+
+export default agenda;
--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import path from 'path';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import agenda from './agenda';
+
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../../client/build')));
 
-app.listen(4000, () => console.info('App running on port 4000'));
+agenda.start().then(() => {
+  app.listen(4000, () => console.info('App running on port 4000'));
+});
