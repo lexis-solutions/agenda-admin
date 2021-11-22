2f242173e27c13f22c6e6129e3eb801862141873
Stefan <stefanmitov8@gmail.com>
2021-11-22 21:22:23 +0100

    update pagination on client side to show page numbers

--- client/src/components/PaginationButtons.tsx
+++ client/src/components/PaginationButtons.tsx
@@ -1,3 +1,5 @@
+import { range } from 'lodash';
+
 interface PropType {
   page: number;
   pagesCount: number;
@@ -12,21 +14,74 @@ const PaginationButtons: React.FC<PropType> = ({
   const handlePrevPage = () => page > 1 && setPage(page - 1);
   const handleNextPage = () => page < pagesCount && setPage(page + 1);
 
+  const setCustomPage = (newPage: number) => {
+    if (newPage >= 1 && newPage <= pagesCount) {
+      setPage(newPage);
+    }
+  };
+
+  const setPageFromUserInput = () => {
+    const pg = prompt('Enter the page number: ');
+    const newPage = parseInt(pg!);
+    setCustomPage(newPage);
+  };
+
+  const renderPageButton = (newPage: number) => (
+    <button
+      key={newPage}
+      className="btn btn-outline"
+      onClick={() => setCustomPage(newPage)}
+      disabled={page === newPage}
+    >
+      {newPage}
+    </button>
+  );
+
+  const renderHiddenPagesButton = () => (
+    <button className="btn btn-outline" onClick={setPageFromUserInput}>
+      ...
+    </button>
+  );
+
+
+  const renderPaginationButtons = () => {
+    if (pagesCount <= 10) {
+      return range(1, pagesCount + 1).map((pg) => renderPageButton(pg));
+    }
+
+    const leftHiddenPages = page - 2 > 2;
+    const rightHiddenPages = page + 2 < pagesCount;
+
+    const start = Math.max(2, page - 2);
+    const end = Math.min(pagesCount - 1, page + 2);
+
+    return (
+      <>
+        {renderPageButton(1)}
+        {leftHiddenPages && renderHiddenPagesButton()}
+        {range(start, end + 1).map((pg) => renderPageButton(pg))}
+        {rightHiddenPages && renderHiddenPagesButton()}
+        {renderPageButton(pagesCount)}
+      </>
+    );
+  };
+
   return (
     <div className="btn-group">
       <button
-        className="btn btn-outline btn-wide"
+        className="btn btn-outline"
         onClick={handlePrevPage}
         disabled={page <= 1}
       >
-        Previous Page
+        ‹
       </button>
+      {renderPaginationButtons()}
       <button
-        className="btn btn-outline btn-wide"
+        className="btn btn-outline"
         onClick={handleNextPage}
         disabled={page >= pagesCount}
       >
-        Next Page
+        ›
       </button>
     </div>
   );
