01e498c57d36c35b187a80ccfb8746bca17b57b6
Stefan <stefanmitov8@gmail.com>
2021-12-10 18:10:30 +0100

    Fix issue with tsconfig.json file

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
 const path = require('path');
 
 module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: [path.resolve(__dirname, 'test/setup.ts')],
-  moduleNameMapper: pathsToModuleNameMapper(
-    { 'src/*': ['./src/*'], 'test/*': ['./test/*'] },
-    {
-      prefix: path.resolve(__dirname, '.'),
-    }
-  ),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: path.resolve(__dirname, '.'),
+  }),
 };
