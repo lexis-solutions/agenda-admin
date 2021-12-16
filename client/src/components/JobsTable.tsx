c126168de79c2911396090e230dea30726492972
Stefan <stefanmitov8@gmail.com>
2021-12-16 07:49:53 +0100

    reuse same modals for all jobs

--- client/src/components/JobsTable.tsx
+++ client/src/components/JobsTable.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import JobsTableRow from 'src/components/JobsTableRow';
 import SortableColumnButton from 'src/components/SortableColumnButton';
 import { JobType, SortType } from 'src/types';
+import { formatLocalDateTime } from 'src/utils/formatter';
 
 interface PropsType {
   sortBy: SortType;
@@ -14,64 +15,143 @@ interface PropsType {
 }
 
 const JobsTable: React.FC<PropsType> = ({
+  data,
   sortBy,
   sortDesc,
   setSortBy,
   setSortDesc,
-  data,
   onDeleteJobs,
   onRequeueJobs,
 }) => {
   const [selectAll, setSelectAll] = useState(false);
+  const [modalJob, setModalJob] = useState<JobType>();
+
+  const formattedLastRunAt = formatLocalDateTime(modalJob?.job.lastRunAt);
+  const formattedNextRunAt = formatLocalDateTime(modalJob?.job.nextRunAt);
+  const formattedFailedAt = formatLocalDateTime(modalJob?.job.failedAt);
 
   return (
-    <table className="table w-full table-zebra">
-      <thead>
-        <tr>
-          <th>
-            <input
-              type="checkbox"
-              className="checkbox"
-              checked={selectAll}
-              onChange={() => setSelectAll(!selectAll)}
+    <div>
+      <table className="table w-full table-zebra">
+        <thead>
+          <tr>
+            <th>
+              <input
+                type="checkbox"
+                className="checkbox"
+                checked={selectAll}
+                onChange={() => setSelectAll(!selectAll)}
+              />
+            </th>
+            <th>Status</th>
+            <th>Name</th>
+            <th>
+              <SortableColumnButton
+                title="Next Run At"
+                column="nextRunAt"
+                currentColumn={sortBy}
+                setCurrentColumn={setSortBy}
+                sortDesc={sortDesc}
+                setSortDesc={setSortDesc}
+              />
+            </th>
+            <th>
+              <SortableColumnButton
+                title="Last Run At"
+                column="lastRunAt"
+                currentColumn={sortBy}
+                setCurrentColumn={setSortBy}
+                sortDesc={sortDesc}
+                setSortDesc={setSortDesc}
+              />
+            </th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((job: JobType) => (
+            <JobsTableRow
+              job={job}
+              key={job?.job._id}
+              setModalJob={setModalJob}
             />
-          </th>
-          <th>Status</th>
-          <th>Name</th>
-          <th>
-            <SortableColumnButton
-              title="Next Run At"
-              column="nextRunAt"
-              currentColumn={sortBy}
-              setCurrentColumn={setSortBy}
-              sortDesc={sortDesc}
-              setSortDesc={setSortDesc}
-            />
-          </th>
-          <th>
-            <SortableColumnButton
-              title="Last Run At"
-              column="lastRunAt"
-              currentColumn={sortBy}
-              setCurrentColumn={setSortBy}
-              sortDesc={sortDesc}
-              setSortDesc={setSortDesc}
-            />
-          </th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((job: JobType) => (
-          <JobsTableRow
-            job={job}
-            key={job.job._id}
-            onDeleteJobs={onDeleteJobs}
-            onRequeueJobs={onRequeueJobs}
-          />
-        ))}
-      </tbody>
-    </table>
+          ))}
+        </tbody>
+      </table>
+
+      {/* Info modal */}
+      <div id="job-data" className="modal">
+        <div className="space-y-4 modal-box">
+          <div className="text-2xl">{`Job data - ${modalJob?.job.name}`}</div>
+          <div>Last Run At: {formattedLastRunAt}</div>
+          <div>Next Run At: {formattedNextRunAt}</div>
+          <div>Job Data:</div>
+          <div className="textarea textarea-bordered">
+            <pre className="overflow-scroll">
+              <code>{JSON.stringify(modalJob?.job.data, null, 2)}</code>
+            </pre>
+          </div>
+          {modalJob?.job.failCount && (
+            <>
+              <div className="font-bold text-red-500">
+                {`Fail Count: ${modalJob?.job.failCount}`}
+              </div>
+              <div className="font-bold text-red-500">
+                {`Reason: ${modalJob?.job.failReason}`}
+              </div>
+              <div className="font-bold text-red-500">
+                Failed At: {formattedFailedAt}
+              </div>
+            </>
+          )}
+          <div className="modal-action">
+            <a href="#!" className="btn">
+              Close
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Requeue modal */}
+      <div id="requeue-job" className="modal">
+        <div className="space-y-4 modal-box">
+          <div className="text-2xl">Requeue job</div>
+          <div>{`ID: ${modalJob?.job._id}`}</div>
+          <div>{`Name: ${modalJob?.job.name}`}</div>
+          <div className="modal-action">
+            <a
+              href="#!"
+              className="btn btn-primary"
+              onClick={() => modalJob && onRequeueJobs([modalJob.job._id])}
+            >
+              Requeue
+            </a>
+            <a href="#!" className="btn">
+              Close
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Delete modal */}
+      <div id="delete-job" className="modal">
+        <div className="space-y-4 modal-box">
+          <div className="text-2xl">Delete job</div>
+          <div>{`ID: ${modalJob?.job._id}`}</div>
+          <div>{`Name: ${modalJob?.job.name}`}</div>
+          <div className="modal-action">
+            <a
+              href="#!"
+              className="btn btn-warning"
+              onClick={() => modalJob && onDeleteJobs([modalJob?.job._id])}
+            >
+              Delete
+            </a>
+            <a href="#!" className="btn">
+              Close
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/src/components/JobsTableRow.tsx
+++ client/src/components/JobsTableRow.tsx
@@ -11,8 +11,7 @@ import Trash from 'src/svgs/Trash';
 
 interface PropsType {
   job: JobType;
-  onDeleteJobs: (ids: string[]) => void;
-  onRequeueJobs: (ids: string[]) => void;
+  setModalJob: (job: JobType) => void;
 }
 
 const statusColors = {
@@ -24,11 +23,7 @@ const statusColors = {
   running: 'bg-yellow-500',
 };
 
-const JobsTableRow: React.FC<PropsType> = ({
-  job,
-  onDeleteJobs,
-  onRequeueJobs,
-}) => {
+const JobsTableRow: React.FC<PropsType> = ({ job, setModalJob }) => {
   const [checked, setChecked] = useState(false);
 
   const relativeLastRunAt = formatRelativeDateTime(job.job.lastRunAt);
@@ -36,7 +31,6 @@ const JobsTableRow: React.FC<PropsType> = ({
 
   const formattedLastRunAt = formatLocalDateTime(job.job.lastRunAt);
   const formattedNextRunAt = formatLocalDateTime(job.job.nextRunAt);
-  const formattedFailedAt = formatLocalDateTime(job.job.failedAt);
 
   return (
     <tr>
@@ -83,89 +77,29 @@ const JobsTableRow: React.FC<PropsType> = ({
       <td>
         <div className="flex flex-wrap">
           {/* Info button */}
-          <a href={`#job-data@${job.job._id}`} className="btn btn-ghost">
+          <a
+            href="#job-data"
+            className="btn btn-ghost"
+            onClick={() => setModalJob(job)}
+          >
             <Info />
           </a>
           {/* Requeue button */}
-          <a href={`#requeue-job@${job.job._id}`} className="btn btn-ghost">
+          <a
+            href="#requeue-job"
+            className="btn btn-ghost"
+            onClick={() => setModalJob(job)}
+          >
             <Refresh />
           </a>
           {/* Delete button */}
-          <a href={`#delete-job@${job.job._id}`} className="btn btn-ghost">
+          <a
+            href="#delete-job"
+            className="btn btn-ghost"
+            onClick={() => setModalJob(job)}
+          >
             <Trash />
           </a>
-          {/* Info modal */}
-          <div id={`job-data@${job.job._id}`} className="modal">
-            <div className="space-y-4 modal-box">
-              <div className="text-2xl">{`Job data - ${job.job.name}`}</div>
-              <div>Last Run At: {formattedLastRunAt}</div>
-              <div>Next Run At: {formattedNextRunAt}</div>
-              <div>Job Data:</div>
-              <div className="textarea textarea-bordered">
-                <pre className="overflow-scroll">
-                  <code>{JSON.stringify(job.job.data, null, 2)}</code>
-                </pre>
-              </div>
-              {job.job.failCount && (
-                <>
-                  <div className="font-bold text-red-500">
-                    {`Fail Count: ${job.job.failCount}`}
-                  </div>
-                  <div className="font-bold text-red-500">
-                    {`Reason: ${job.job.failReason}`}
-                  </div>
-                  <div className="font-bold text-red-500">
-                    Failed At: {formattedFailedAt}
-                  </div>
-                </>
-              )}
-              <div className="modal-action">
-                <a href="#" className="btn">
-                  Close
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Requeue modal */}
-          <div id={`requeue-job@${job.job._id}`} className="modal">
-            <div className="space-y-4 modal-box">
-              <div className="text-2xl">Requeue job</div>
-              <div>{`ID: ${job.job._id}`}</div>
-              <div>{`Name: ${job.job.name}`}</div>
-              <div className="modal-action">
-                <a
-                  href="#"
-                  className="btn btn-primary"
-                  onClick={() => onRequeueJobs([job.job._id])}
-                >
-                  Requeue
-                </a>
-                <a href="#" className="btn">
-                  Close
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Delete modal */}
-          <div id={`delete-job@${job.job._id}`} className="modal">
-            <div className="space-y-4 modal-box">
-              <div className="text-2xl">Delete job</div>
-              <div>{`ID: ${job.job._id}`}</div>
-              <div>{`Name: ${job.job.name}`}</div>
-              <div className="modal-action">
-                <a
-                  href="#"
-                  className="btn btn-warning"
-                  onClick={() => onDeleteJobs([job.job._id])}
-                >
-                  Delete
-                </a>
-                <a href="#" className="btn">
-                  Close
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
       </td>
     </tr>
