56630834bf9fd52ac47c960c408643943c388762
Stefan <stefanmitov8@gmail.com>
2021-12-21 21:05:07 +0100

    fix overview query

--- api/src/controllers/overview.ts
+++ api/src/controllers/overview.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import agenda from 'src/agenda';
-import { buildGetJobsQuery } from 'src/utils/build-get-jobs-query';
+import { buildJobQuery } from 'src/utils/build-job-query';
 
 interface ReqQuery {
   name: string | null;
@@ -13,7 +13,7 @@ export const overview = async (
   res: Response,
   next: NextFunction
 ) => {
-  const [query, , projectStatuses] = buildGetJobsQuery({
+  const { query } = buildJobQuery({
     ...req.query,
     status: null,
     sortBy: 'lastRunAt',
@@ -22,8 +22,91 @@ export const overview = async (
 
   const data = await agenda._collection
     .aggregate([
-      query,
-      projectStatuses,
+      { $match: query },
+      {
+        $project: {
+          running: {
+            $sum: {
+              $cond: [
+                {
+                  $and: [
+                    '$lastRunAt',
+                    { $gt: ['$lastRunAt', '$lastFinishedAt'] },
+                  ],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          scheduled: {
+            $sum: {
+              $cond: [
+                {
+                  $and: ['$nextRunAt', { $gte: ['$nextRunAt', new Date()] }],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          queued: {
+            $sum: {
+              $cond: [
+                {
+                  $and: [
+                    '$nextRunAt',
+                    { $gte: [new Date(), '$nextRunAt'] },
+                    { $gte: ['$nextRunAt', '$lastFinishedAt'] },
+                  ],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          completed: {
+            $sum: {
+              $cond: [
+                {
+                  $and: [
+                    '$lastFinishedAt',
+                    { $gt: ['$lastFinishedAt', '$failedAt'] },
+                  ],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          failed: {
+            $sum: {
+              $cond: [
+                {
+                  $and: [
+                    '$lastFinishedAt',
+                    '$failedAt',
+                    { $eq: ['$lastFinishedAt', '$failedAt'] },
+                  ],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          repeating: {
+            $sum: {
+              $cond: [
+                {
+                  $and: ['$repeatInterval', { $ne: ['$repeatInterval', null] }],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+        },
+      },
       {
         $group: {
           _id: 1,
