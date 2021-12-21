42391a4cc70688ea9535e34a95dc93de5ac78f3b
Stefan <stefanmitov8@gmail.com>
2021-12-21 20:22:01 +0100

    Add routes to delete and requeue jobs by query

--- api/src/controllers/overview.ts
+++ api/src/controllers/overview.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import agenda from 'src/agenda';
-import { buildJobQuery } from 'src/utils/build-job-query';
+import { buildGetJobsQuery } from 'src/utils/build-get-jobs-query';
 
 interface ReqQuery {
   name: string | null;
@@ -13,7 +13,7 @@ export const overview = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { query } = buildJobQuery({
+  const [query, , projectStatuses] = buildGetJobsQuery({
     ...req.query,
     status: null,
     sortBy: 'lastRunAt',
@@ -22,91 +22,8 @@ export const overview = async (
 
   const data = await agenda._collection
     .aggregate([
-      { $match: query },
-      {
-        $project: {
-          running: {
-            $sum: {
-              $cond: [
-                {
-                  $and: [
-                    '$lastRunAt',
-                    { $gt: ['$lastRunAt', '$lastFinishedAt'] },
-                  ],
-                },
-                1,
-                0,
-              ],
-            },
-          },
-          scheduled: {
-            $sum: {
-              $cond: [
-                {
-                  $and: ['$nextRunAt', { $gte: ['$nextRunAt', new Date()] }],
-                },
-                1,
-                0,
-              ],
-            },
-          },
-          queued: {
-            $sum: {
-              $cond: [
-                {
-                  $and: [
-                    '$nextRunAt',
-                    { $gte: [new Date(), '$nextRunAt'] },
-                    { $gte: ['$nextRunAt', '$lastFinishedAt'] },
-                  ],
-                },
-                1,
-                0,
-              ],
-            },
-          },
-          completed: {
-            $sum: {
-              $cond: [
-                {
-                  $and: [
-                    '$lastFinishedAt',
-                    { $gt: ['$lastFinishedAt', '$failedAt'] },
-                  ],
-                },
-                1,
-                0,
-              ],
-            },
-          },
-          failed: {
-            $sum: {
-              $cond: [
-                {
-                  $and: [
-                    '$lastFinishedAt',
-                    '$failedAt',
-                    { $eq: ['$lastFinishedAt', '$failedAt'] },
-                  ],
-                },
-                1,
-                0,
-              ],
-            },
-          },
-          repeating: {
-            $sum: {
-              $cond: [
-                {
-                  $and: ['$repeatInterval', { $ne: ['$repeatInterval', null] }],
-                },
-                1,
-                0,
-              ],
-            },
-          },
-        },
-      },
+      query,
+      projectStatuses,
       {
         $group: {
           _id: 1,
--- api/src/controllers/requeue-jobs.ts
+++ api/src/controllers/requeue-jobs.ts
@@ -1,12 +1,41 @@
 import { NextFunction, Request, Response } from 'express';
 import agenda from 'src/agenda';
 import { ObjectId } from 'mongodb';
+import { StatusType } from 'src/types';
+import { buildGetJobsQuery } from 'src/utils/build-get-jobs-query';
+
+interface ReqQuery {
+  sortBy: 'lastRunAt' | 'nextRunAt';
+  sortType: 'desc' | 'asc';
+  page: number;
+  itemsPerPage: number;
+  status: StatusType | null;
+  name: string | null;
+  property: string | null;
+  value: any | null;
+}
 
 interface ReqBody {
   ids: string[];
 }
 
-export const requeueJobs = async (
+export const requeueJobsByQuery = async (
+  req: Request<any, any, any, ReqQuery>,
+  res: Response,
+  next: NextFunction
+) => {
+  const query = buildGetJobsQuery(req.query);
+  const jobs = await agenda._collection.aggregate([...query]).toArray();
+  const requeuedJobs = await Promise.all(
+    jobs.map(({ job }) => agenda.now(job.name, job.data))
+  );
+
+  res.locals.requeued = requeuedJobs;
+
+  next();
+};
+
+export const requeueJobsById = async (
   req: Request<any, any, ReqBody, any>,
   res: Response,
   next: NextFunction
