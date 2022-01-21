6ecaab24eff26564814d677f612f6be80598abf2
Stefan <stefanmitov8@gmail.com>
2022-01-21 10:14:10 +0100

    middleware function improvements

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,17 +7,23 @@ import { Express } from 'express';
 import { setAgendaInstance } from 'src/agenda-instance';
 import app from 'src/app';
 
-export const mountAgendaAdmin = async (
-  url: string,
-  expressApp: Express,
-  agenda: Agenda
-) => {
-  const parsedUrl = new URL(url);
+interface ParamsType {
+  publicUrl: string;
+  expressApp: Express;
+  agenda: Agenda;
+}
+
+export const mountAgendaAdmin = async ({
+  publicUrl,
+  expressApp,
+  agenda,
+}: ParamsType) => {
+  const parsedUrl = new URL(publicUrl);
 
   const indexHtml = path.join(__dirname, '../../client/build/index.html');
   const updatedIndexHtml = fs
     .readFileSync(indexHtml, 'utf-8')
-    .replace(/__PUBLIC_URL_PLACEHOLDER__/g, url);
+    .replace(/__PUBLIC_URL_PLACEHOLDER__/g, publicUrl);
   fs.writeFileSync(indexHtml, updatedIndexHtml);
 
   const directory = path.join(__dirname, '../../client/build/static/js');
@@ -26,20 +32,24 @@ export const mountAgendaAdmin = async (
     .filter((file) => file.endsWith('.js'))
     .map((fileName) => path.join(directory, fileName));
 
-  for (const file of files) {
-    const tmpFile = `${file}.tmp`;
-    await new Promise((resolve, reject) => {
-      const stream = fs
-        .createReadStream(file)
-        .pipe(replaceStream('__PUBLIC_URL_PLACEHOLDER__', url))
-        .pipe(fs.createWriteStream(tmpFile));
-      stream.on('finish', resolve);
-      stream.on('error', reject);
-    });
-    fs.unlinkSync(file);
-    fs.copyFileSync(tmpFile, file);
-    fs.unlinkSync(tmpFile);
-  }
+  await Promise.all(
+    files.map((file: string) =>
+      (async () => {
+        const tmpFile = `${file}.tmp`;
+        await new Promise((resolve, reject) => {
+          const stream = fs
+            .createReadStream(file)
+            .pipe(replaceStream('__PUBLIC_URL_PLACEHOLDER__', publicUrl))
+            .pipe(fs.createWriteStream(tmpFile));
+          stream.on('finish', resolve);
+          stream.on('error', reject);
+        });
+        fs.unlinkSync(file);
+        fs.copyFileSync(tmpFile, file);
+        fs.unlinkSync(tmpFile);
+      })()
+    )
+  );
 
   setAgendaInstance(agenda);
   expressApp.use(parsedUrl.pathname, app);
